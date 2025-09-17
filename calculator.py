b611ff65b71e2a60edae9174dc4635fd1064fa1d
Usman <udandina@yahoo.com>
2025-09-17 22:45:01 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+num1 = input("Enter First Number: ")
+operator = input("Select Operator, +, -, *, /")
+num2 = input("Enter Second Number: ")
+
+if (operator == +){
+  print(num1+num2)
+}
+    elif (operator == -){
+  print(num1-num2)
+    }
+    elif (operator == *){
+      print(num1*num2)
+    }
+      elif (operator == /){
+        print(num1/num2)    
+    }
+else{
+  print("Select a valid operator")
+}
+          
